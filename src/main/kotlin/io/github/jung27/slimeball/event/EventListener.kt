e4f6dff782bf8090d2194b6228e61562f4137386
jung27 <lglg800315@gmail.com>
2023-03-27 18:14:17 +0900

    :sparkles: 슬라임볼 소모품으로 변경, 팔 휘두르는 모션 추가

--- src/main/kotlin/io/github/jung27/slimeball/event/EventListener.kt
+++ src/main/kotlin/io/github/jung27/slimeball/event/EventListener.kt
@@ -1,36 +1,33 @@
 package io.github.jung27.slimeball.event
 
 import io.github.jung27.slimeball.SlimePool
-import io.github.jung27.slimeball.plugin.SlimeballPlugin
-import net.kyori.adventure.text.Component.text
-import net.kyori.adventure.text.format.NamedTextColor
-import org.bukkit.Bukkit
-import org.bukkit.Color
+import org.bukkit.GameMode
 import org.bukkit.Material
 import org.bukkit.entity.Snowball
-import org.bukkit.entity.TextDisplay
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.block.Action
 import org.bukkit.event.entity.ProjectileHitEvent
 import org.bukkit.event.player.PlayerInteractEvent
 import org.bukkit.inventory.ItemStack
-import org.bukkit.util.Transformation
 import org.bukkit.util.Vector
-import org.joml.Quaternionf
-import org.joml.Vector3f
 import java.util.Random
 
 class EventListener : Listener {
     @EventHandler
     fun onRightClick(event: PlayerInteractEvent){
-        if(event.action == Action.RIGHT_CLICK_AIR){
+        if(event.action == Action.RIGHT_CLICK_AIR || event.action == Action.RIGHT_CLICK_BLOCK){
             if(event.item?.type == Material.SLIME_BALL){
+                val player = event.player
                 val random = Random()
-                event.player.launchProjectile(Snowball::class.java, event.player.location.direction
+                player.launchProjectile(Snowball::class.java, player.location.direction
                     .add(Vector((0.5-random.nextDouble())*0.1, 0.0, (0.5-random.nextDouble())*0.1))) {
                     it.item = ItemStack(Material.SLIME_BALL)
                 }
+                if(player.gameMode == GameMode.ADVENTURE || player.gameMode == GameMode.SURVIVAL){
+                    event.item?.amount = event.item?.amount?.minus(1) ?: 0
+                }
+                player.swingMainHand()
             }
         }
     }
@@ -38,8 +35,11 @@ class EventListener : Listener {
     @EventHandler
     fun onProjectileHit(event: ProjectileHitEvent){
         val entity = event.entity
-        if (entity is Snowball && entity.item?.type == Material.SLIME_BALL) {
-            val location = entity.location.toBlockLocation().add(0.5, 0.0, 0.5)
+        if (entity is Snowball && entity.item.type == Material.SLIME_BALL) {
+            val location = entity.location.toBlockLocation().apply {
+                x = entity.location.x
+                z = entity.location.z
+            }
             SlimePool(location)
         }
     }
